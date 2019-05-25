b5cbc8ede171028a8dd3af9f9af1cfee8d28d623
Jim <a9372275@9372275s-MBP.home>
2019-05-25 14:38:59 +0100

    pick countries now functional

--- src/components/PickCountries.js
+++ src/components/PickCountries.js
@@ -6,82 +6,82 @@ class PickCountries extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedIndex: 0,
+      countryPick: 0,
       filterLetter: null
     }
-    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleCountryChange = this.handleCountryChange.bind(this);
     this.handleSelectLetter = this.handleSelectLetter.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    // this.reset = this.reset.bind(this);
   }
 
   handleFormSubmit(evt) {
     evt.preventDefault();
     const newBucketItem = {
-      country: "XXX", // dummy value for testing
+      country: this.state.countryPick,
       done: false
     }
     this.props.addToBucket(newBucketItem);
-    // this.reset();
   }
 
-  // reset() {
-  //   this.setState({
-  //     selectedIndex: 0,
-  //     filterLetter: null
-  //   });
-  // }
-
-  handleSelectChange(evt) {
-    this.setState({selectedIndex: evt.target.value});
-    // this.props.handleSelectedCountry(evt.target.value);
+  handleCountryChange(evt) {
+    this.setState({countryPick: evt.target.value});
   }
 
-  createOptions() {
+  selectOptions() {
     if (this.state.filterLetter == null) {
       return this.props.countries.map((country, index) => {
         return <option value={index} key={index}>{country.name}</option>
       });
     } else {
-      let countriesArray = []
+      let filteredCountries = []
       for (var i = 0; i < this.props.countries.length; i++) {
-        let name = this.props.countries[i].name.slice(0,1)
-        if (name == this.state.filterLetter) {
-          let country = {name: this.props.countries[i], index: i}
-          countriesArray.push(country)
-        }
-      }
-      for (let country of this.props.countries) {
-
-      }
-      return countriesArray.map((country) => {
-
-        return <option value={country.index} key={country.index}>{country.name.name}</option>
+        let firstLetter = this.props.countries[i].name.slice(0,1)
+        if (firstLetter === this.state.filterLetter) {
+          let country = {details: this.props.countries[i], indexNum: i}
+          filteredCountries.push(country)
+        };
+      };
+      return filteredCountries.map((country) => {
+        return <option value={country.indexNum} key={country.indexNum}>{country.details.name}</option>
       });
-    }
+    };
+  };
+
+  handleSelectLetter(evt) {
+    let keyLetter = evt.target.value;
+    let keyIndex = 0;
+    for (var i = 0; i < this.props.countries.length; i++) {
+      let firstLetter = this.props.countries[i].name.slice(0,1)
+      if (firstLetter === keyLetter) {
+        keyIndex = i;
+        break;
+      };
+    };
+    this.setState({filterLetter: keyLetter, countryPick: keyIndex});
   }
 
-  createLetters() {
+  filterLetters() {
     let letters=("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split("")
     return letters.map((letter, index) => {
       return <option value={letter} key={index} onClick={this.handleSelectLetter}>{letter}</option>
     });
   }
 
-  handleSelectLetter(evt) {
-    this.setState({filterLetter: evt.target.value});
-  }
-
   render() {
     return (
       <>
       <div className='flex'>
-        {this.createLetters()}
+        {this.filterLetters()}
       </div>
 
       <form onSubmit={this.handleFormSubmit} autoComplete='off'>
-        <select value={this.state.selectedIndex} onChange={this.handleSelectChange}>
-          {this.createOptions()}
+        <select
+          name='countryPick'
+          id='countryPick'
+          value={this.state.countryPick}
+          onChange={this.handleCountryChange}
+        >
+          {this.selectOptions()}
         </select>
         <input type='submit' value='Add To List' />
       </form>
